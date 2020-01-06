354902dbdfc69a495f299a772e4bfb7230582f86
David Koeller <david.koeller@gmail.com>
2020-01-06 17:31:41 -0600

    psuedo coded app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+// Create base variables.
+
+// Create questions in an array.  
+    // For each question create an object.
+        // Question: "string"
+        // Answers: [Array]
+        // Correct Answer: Array Index
+
+// On click function that begins the quiz.
+    //clicking the button also starts a 30 second timer.
+    //clicking the button also hides the instructions and replaces it with the question.
+
+//Have it show the time in the HTML
+
+// Create the Game Function.
+    //On click events for the answer buttons.
+    //if correct answer is clicked +1 to the correct answer variable.
+    //if incorrect answer is clicked +1 to incorrect answer variable.
+    //if time expires +1 to incorrect answer variable.
+
+    //when a button is clicked or time expires:
+        //show correct answer and if the player got it right. (show for about 3-5 seconds).
+        //move on the the next question and reset the timer.
+
+
+//write the updated score to the HTML.
+//After running through the entire array.  The game is finished.
+
+
