95cb1a06deb2ef66993c504ee476ac5713668239
David Koeller <david.koeller@gmail.com>
2020-01-09 21:11:31 -0600

    some of the game works; needs styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,11 @@ var incorrectAnswers = 0;
 
 var question = 0;
 
+var count = 5;
 
+var counter;
+
+var userGuess;
 
 
 
@@ -18,7 +22,7 @@ var quizQuestions = [
     {
         question: "What is the U.S.S. Enterprise's registry number?",
         options: ["NCC-1701", "NCC-1071", "NX-01", "NCC-1700"],
-        correctAnswer: 0
+        correctAnswer: "NCC-1701"
     },
     {
         question: "In the episode Trouble with Tribbles, why did Scotty start a fight with the Klingons?",
@@ -30,15 +34,107 @@ var quizQuestions = [
         options: [""],
         correctAnswer: ""
     }
-]
+];
+
+var correctAnswer = quizQuestions[0].correctAnswer;
+// On click function that begins the quiz.
+$("#start-button").on("click", function(){
+   //clicking the button hides instructions.
+    $("#main-info").hide();
+    //clicking the button also starts a 30 second timer and starts the game.
+    counter = setInterval(timer, 1000);
+    //runs the game function.
+    triviaGame();
     
+
+})
+//Timer function
+function timer() {
+    count--;
+    $("#timer").html("Time Left: " + count);
+    if (count ===0) {
+        incorrectAnswers++;
+        $("#question-div").hide();
+        $("#options-div").hide();
+        $("#timer").hide();
+        var timeoutMsg = $("<div>");
+        timeoutMsg.addClass("timeout-msg");
+        $("#information").html(timeoutMsg);
+        $(".timeout-msg").text("Uh oh!  You ran out of time!  The correct answer is: " + correctAnswer);
+        clearTimer();
+        }
+    //Have it show the time in the HTML    
+    
+}
+
+function clearTimer() {
+    clearInterval(counter);
+}
+
+// Create the Game Function.
+function triviaGame() {
+    //shows the current question.
+    $("#question-div").text(quizQuestions[0].question);
+    question++;
+    //create a variable for the answer options.
+    var optionsArr = quizQuestions[0].options;
+    
+   
+    //Loop through the option array.
+    for (i =0; i < optionsArr.length; i++) {
+        var button = $("<button>");
+        //create a button for every option in the array.
+        button.text(optionsArr[i]);
+        //give buttons data- attribute
+        button.attr("data-guess", optionsArr[0]);
+        //add class to buttons
+        $(button).addClass("button-guess");
+        //display the options as buttons in the html.
+        $("#options-div").append(button);
+       console.log(optionsArr[i]);
+    }
+    //create an on click event for the answer buttons.
+    $("#options-div").on("click", ".button-guess", function() {
+        var userGuess = $(this).data("guess");
+        var correctAnswer = quizQuestions[0].correctAnswer;
+        console.log(correctAnswer);
+        console.log(userGuess);
+        if( userGuess === correctAnswer) {
+            correctAnswers++;
+            $("#question-div").hide();
+            $("#options-div").hide();
+            $("#timer").hide();
+            var correctMsg = $("<div>");
+            correctMsg.addClass("correct-msg");
+            $("#information").html(correctMsg);
+            $(".correct-msg").text("Congratulations! The correct answer is: " + correctAnswer);
+            console.log("Right Answer!");
+            
+        }
+        else {
+            incorrectAnswers++;
+            $("#question-div").hide();
+            $("#options-div").hide();
+            $("#timer").hide();
+            var wrongMsg = $("<div>");
+            wrongMsg.addClass("wrong-msg");
+            $("#information").html(wrongMsg);
+            $(".wrong-msg").text("Wrong Answer! The correct answer is: " + correctAnswer);
+            console.log("Wrong Answer!");
+        }
+    })
+}
+
+function newQuestion () {
+    $
+}
+
+
         
 
-// On click function that begins the quiz.
-    //clicking the button also starts a 30 second timer.
-    //clicking the button also hides the instructions and replaces it with the question.
 
-//Have it show the time in the HTML
+
+
 
 // Create the Game Function.
     //On click events for the answer buttons.
